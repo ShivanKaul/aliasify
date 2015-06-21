d531f4e366d1ec273c2c8366e56d9386ccdb9b64
ShivanKaul <shivan@omsignal.com>
2015-06-21 12:56:23 -0400

    Pushed first version out. Supporting colorized output.

--- aliasify.gemspec
+++ aliasify.gemspec
@@ -10,17 +10,11 @@ Gem::Specification.new do |spec|
   spec.email         = ["shivankaulsahib@gmail.com"]
 
   spec.summary       = %q{Add an alias to your current directory.}
-  spec.description   = %q{Tired of changing directories often to commonly used projects? Just type `aliasify <alias-name>` and the gem will create an alias to the current working directory.}
-  spec.homepage      = "http://shivankaulsahib.me"
+  spec.description   = <<-EOF
+  Tired of changing directories often to commonly used projects? Just type `aliasify <alias-name>` and the gem will create an alias to the current working directory. Example: `aliasify abc`. Now, simply type in `abc` wherever you are and navigate to the present directory.
+  EOF
+  spec.homepage      = 'http://rubygems.org/gems/aliasify'
   spec.license       = "MIT"
-
-  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
-  # delete this section to allow pushing this gem to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
-  end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.executables   = [ "aliasify" ]
--- lib/aliasify.rb
+++ lib/aliasify.rb
@@ -8,7 +8,7 @@ module Aliasify
 		cwd = Dir.pwd
 		@@toAdd = "alias #{shortcut}=\"cd #{cwd}\""
 		@@shortcut = shortcut
-		print "Trying to add `#{@@toAdd}`."
+		puts green("[aliasify]") + " Trying to add the alias to some kind of login script..."
 		if file_exists(".bash_aliases")
 			add_to_file(".bash_aliases")
 		elsif file_exists(".bash_profile")
@@ -17,25 +17,36 @@ module Aliasify
 			add_to_file(".bashrc")
 		elsif file_exists(".bash_login")
 			add_to_file(".bash_login")
+		elsif file_exists(".profile")
+			add_to_file(".profile")
 		elsif file_exists(".environment")
 			add_to_file(".environment")
-		else print "Tried to look for .bash_aliases, .bash_profile, .bashrc, .bash_login and .environment.
-			None of them were found, so won't be able to add alias :("
+		else print green("[aliasify]") + red(" Tried to find .bash_aliases, .bash_profile, .bashrc, .bash_login , .login, and .environment. None of them were found, so won't be able to add alias :(")
 		end
 	end
 
 	# Helper Functions
 
 	def self.add_to_file(filename)
-		File.open(filename, 'a') do |file|
+		File.open("#{Dir.home}/#{filename}", 'a') do |file|
 			file.puts @@toAdd
-			puts " Found #{filename}."
-			print "Success! Go to the current directory from wherever by simply typing `#{@@shortcut}`."
+			puts green("[aliasify]") + " Found " + yellow("#{filename}")
+			print green("[aliasify]") + " Success! Now, run `" + green("source ~/#{filename}") + "` to make the alias functional. Then, go to the current directory from wherever by simply typing in `" + green("#{@@shortcut}") + "`."
 		end
 	end
 
 	def self.file_exists(filename)
 		File.exist?("#{Dir.home}/#{filename}")
 	end
+
+	# Citation: http://stackoverflow.com/questions/2070010/how-to-output-my-ruby-commandline-text-in-different-colours/2070405#2070405
+	# Didn't want to include a dependency on another gem
+	def self.colorize(text, color_code)
+		"\e[#{color_code}m#{text}\e[0m"
+	end
+
+	def self.red(text); colorize(text, 31); end
+	def self.green(text); colorize(text, 32); end
+	def self.yellow(text); colorize(text, 33); end
 
 end
